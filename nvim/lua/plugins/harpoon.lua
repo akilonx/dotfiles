c05847f25781f1617ccdda15b10c1c89bfb1383e
Akilon <akilon@bookipi.com>
2024-12-13 13:20:41 +0800

    feat: upgrade harpoon2

--- nvim/lua/plugins/harpoon.lua
+++ nvim/lua/plugins/harpoon.lua
@@ -1,13 +1,14 @@
 return {
   "ThePrimeagen/harpoon",
-  dependencies = { "nvim-telescope/telescope.nvim" },
+  branch = "harpoon2",
+  dependencies = { "nvim-lua/plenary.nvim", "nvim-telescope/telescope.nvim" },
   config = function()
     require("harpoon").setup({
       global_settings = {
         save_on_toggle = true,
         save_on_change = true,
-        enter_on_sendcmd = false,
-        tmux_autoclose_windows = false,
+        -- enter_on_sendcmd = false,
+        -- tmux_autoclose_windows = false,
         excluded_filetypes = { "harpoon" },
       },
     })
